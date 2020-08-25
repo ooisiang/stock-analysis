c1fdab1bd0632069529460c0d5b912136136597b
Ooi Siang Tan <ooisiangtan@gmail.com>
2020-08-25 22:55:28 +0200

    feat: Add get_stock_prices() to find matching stock price from StockPricesTable
    Stock prices are found by matching Symbol and fiscalDateEnding.
    
    Signed-off-by: Ooi Siang Tan <ooisiangtan@gmail.com>

--- calc_financial_ratios.py
+++ calc_financial_ratios.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import pandas as pd
+import numpy as np
+import datetime as dt
 
 sys.path.append(os.path.abspath(os.path.join('..')))
 from import_data import load_existing_data, save_data
@@ -33,6 +35,54 @@ def get_basic_eps(df_financial_statements, report_type):
     return df_eps
 
 
+def get_stock_prices(financial_ratios, stock_prices, stock_price_type, date_offset):
+    """
+    This function aims to get the stock price of a given date in each entry in financial_ratios.
+    The dates are the values in the given column, date_col in financial_ratios.
+    The stock price can be found by matching the date and ticker symbol in the stock_prices.
+    User can also add an offset to the date to achieve a stock price on a different day based on the date in date_col.
+
+    :param financial_ratios: the dataframe, df_financial_ratios constructed in calc_financial_ratios
+    :type financial_ratios: pd.DataFrame
+    :param stock_prices: the dataframe load from StockPricesTable in the database
+    :type stock_prices: pd.DataFrame
+    :param stock_price_type: type of stock price to retrieve (col name in stock_prices)
+    :type stock_price_type: str
+    :param date_offset: number of days to offset
+    :type date_offset: int
+    :return: a list of stock prices with the same number of columns as in financial_ratios
+    :rtype: list
+    """
+
+    stock_price_list = []
+
+    # loop through all entries in financial_ratios to find the matching stock price
+    for idx in range(financial_ratios.shape[0]):
+        symbol = financial_ratios.iloc[idx].Symbol
+        # get the stock price on the fiscal ending date in the entry or a date offset set by the user
+        date = financial_ratios.iloc[idx].fiscalDateEnding + dt.timedelta(days=date_offset)
+        stock_price = stock_prices[(stock_prices.Symbol == symbol)
+                                   & (stock_prices.Date == date)][stock_price_type].values
+
+        # if no stock price is found on this date, try one day later and keep adding one day if not found.
+        while_loop_count = 0
+        while stock_price.size == 0:
+            while_loop_count += 1
+            date += dt.timedelta(days=1)
+            stock_price = stock_prices[(stock_prices.Symbol == symbol)
+                                       & (stock_prices.Date == date)][stock_price_type].values
+
+            # if no stock price is available 5 days later, stop finding and set it to np.nan
+            if while_loop_count > 4:
+                stock_price = [np.nan]
+                break
+
+        # append the found stock price to the list
+        stock_price_list.append(stock_price[0])
+
+    return stock_price_list
+
+
 def calc_financial_ratios(df_financial_statements, df_profile, df_stock_prices, report_type):
     """
     This function aims to calculate the financial ratios for the selected entries (annual or quarterly) in
@@ -50,9 +100,17 @@ def calc_financial_ratios(df_financial_statements, df_profile, df_stock_prices, 
     df_selected_financial_statements = df_financial_statements[df_financial_statements.type == report_type]
     df_financial_ratios = df_selected_financial_statements[['Symbol', 'fiscalDateEnding']]
 
+    # get stock prices based on the 'Symbol' and 'fiscalDateEnding' in df_financial_ratios and append to it
+    print('-> Getting stock prices...')
+    selected_stock_prices = get_stock_prices(df_financial_ratios, df_stock_prices, '4. close', 0)
+    df_financial_ratios.loc[:, 'StockPriceOnFiscalDateEnding'] = selected_stock_prices
+    print('-> Stock prices retrieved!')
+
     # Calculate basic eps and concatenate df_eps to the final dataframe, df_financial_ratios
+    print('-> Getting EPS...')
     df_eps = get_basic_eps(df_selected_financial_statements, report_type)
     df_financial_ratios = pd.concat([df_financial_ratios, pd.DataFrame(df_eps, columns=['EPS'])], axis=1)
+    print('-> EPS calculated!')
 
     return df_financial_ratios
 
@@ -63,14 +121,14 @@ def main():
         financial_data_database_filepath, financial_ratios_database_filepath, report_type = sys.argv[1:]
 
         # load financial data from CompanyData.db
-        print('Loading database...\n    DATABASE: {}'.format(financial_data_database_filepath))
+        print('Loading database...\n->DATABASE: {}'.format(financial_data_database_filepath))
         financial_statements, profile, stock_prices = load_existing_data(financial_data_database_filepath)
 
         print('Calculating financial ratios...')
         financial_ratios = calc_financial_ratios(financial_statements, profile, stock_prices, report_type)
-        print('    Done!')
+        print('Done!')
 
-        print('Saving data...\n    DATABASE: {}'.format(financial_ratios_database_filepath))
+        print('Saving data...\n->DATABASE: {}'.format(financial_ratios_database_filepath))
         save_data(financial_ratios, financial_ratios_database_filepath, 'FinancialRatiosTable')
         print('Database updated!')
 
